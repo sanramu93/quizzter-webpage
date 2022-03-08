631e9d2572a9fc57faf0e1e91a64b7bac7ca067b
Santiago <sanramu93@gmail.com>
2022-03-08 14:13:38 -0500

    Add testimonials animations

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const userImgs = document.querySelectorAll(".user-img");
+const testimonials = document.querySelectorAll(".testimonial-text-p");
 
 userImgs.forEach((img) => {
   img.addEventListener("click", (e) => {
     userImgs.forEach((img) => {
       img.classList.remove("user-img--active");
     });
+    testimonials.forEach((testimonial) => {
+      testimonial.classList.remove("testimonial-text-p--active");
+    });
 
     e.target.classList.add("user-img--active");
+
+    const testimonialAtt = img.dataset.testimonial;
+
+    const testimonial = document.querySelector(
+      `[data-testimonial='${testimonialAtt}']`
+    );
+    testimonial.classList.add("testimonial-text-p--active");
   });
 });
